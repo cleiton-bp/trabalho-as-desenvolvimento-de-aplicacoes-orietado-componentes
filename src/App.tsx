ece90721302aa0ca9dc64bb2c7105998b2c2cdf1
Gustavo Daitx <gustavo-daitx@rede.ulbra.br>
2025-12-01 19:45:23 -0300

    Configuração Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,15 @@
-import { useState } from "react";
 import "./App.css";
-import Modal from "./components/modal.component";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./pages/home";
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  return (
-    <>
-      <div className="flex h-screen flex-col justify-center items-center bg-gray-100 gap-4">
-        <div className="row">
-          <h1 className="text-4xl font-bold text-blue-600 text-center">
-            Hello, Vite with React and Tailwind CSS!
-          </h1>
-        </div>
-
-        <div className="row">
-          <button
-            onClick={() => setIsModalOpen(true)}
-            className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors"
-          >
-            <span className="flex items-center justify-center text-base font-semibold">
-              Adicionar Participante
-            </span>
-          </button>
-        </div>
-      </div>
-
-      {/* Modal: adicionar participante */}
-      <Modal
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        title="Adicionar Participante"
-        size="lg"
-      >
-        <div className="p-6">teste</div>
-      </Modal>
-    </>
+    return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
